dfd11fd47ce6fc15ae9b90a46bf05fe32ba3a12e
정민경 <jungmk@habitfactory.co>
2025-09-09 15:50:44 +0900

    Feat: DB 조작하기 위한 주석 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,46 +1,36 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
-import { v1 as uuid } from 'uuid';
-import { CreateBoardDto } from './dto/create-board.dto';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
-
-  getAllBoards(): Board[] {
-    return this.boards;
-  }
-
-  createBoard(createBoardDto: CreateBoardDto) {
-    const { title, description } = createBoardDto;
-
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-    this.boards.push(board);
-    return board;
-  }
-
-  // 못찾으면 NotFoundException 던지기
-  getBoardById(id: string): Board {
-    const found = this.boards.find((board) => board.id === id);
-    if (!found) {
-      throw new NotFoundException(`Board with ID "${id}" not found`);
-    }
-    return found;
-  }
-
-  deleteBoard(id: string): void {
-    const found = this.getBoardById(id); // 해당 id의 게시물이 있는지 확인
-    this.boards = this.boards.filter((board) => board.id !== found.id); // id가 다른 것들만 남기기
-  }
-
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
+  // getAllBoards(): Board[] {
+  //   return this.boards;
+  // }
+  // createBoard(createBoardDto: CreateBoardDto) {
+  //   const { title, description } = createBoardDto;
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  // // 못찾으면 NotFoundException 던지기
+  // getBoardById(id: string): Board {
+  //   const found = this.boards.find((board) => board.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Board with ID "${id}" not found`);
+  //   }
+  //   return found;
+  // }
+  // deleteBoard(id: string): void {
+  //   const found = this.getBoardById(id); // 해당 id의 게시물이 있는지 확인
+  //   this.boards = this.boards.filter((board) => board.id !== found.id); // id가 다른 것들만 남기기
+  // }
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   const board = this.getBoardById(id);
+  //   board.status = status;
+  //   return board;
+  // }
 }
